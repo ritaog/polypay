15277b4bbf275c8eb15370de0b39074573ab085a
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2022-01-09 17:39:54 -0700

    Added client folder

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Welcome = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("http://localhost:5000/api/welcome");
+      setMessage(response.data);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Polypay says: {message}</h1>
+    </div>
+  );
+};
+
+export default Welcome;
