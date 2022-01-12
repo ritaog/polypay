e13da6e30c4f34a245244475386441273edbdd67
theowall <theo.wall11@gmail.com>
2022-01-12 12:30:54 -0700

    fixing form formatting

--- client/src/components/ProfilePageForm.js
+++ client/src/components/ProfilePageForm.js
@@ -6,27 +6,27 @@ const ProfilePageForm = () => {
     <div>
             This is ProfilePageForm
             
-        <div>
-    <form>
-    <label htmlFor="name">Name</label>
-    <input type="text" id="name" name="name" placeholder="Your name.."/>
+        <div className="profile-page-form">
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Your name.."/>
 
-    <label htmlFor="cname">Company Name </label>
-    <input type="text" id="cname" name="companyname" placeholder="Company name.."/>
+                <label htmlFor="cname">Company Name </label>
+                <input type="text" id="cname" name="companyname" placeholder="Company name.."/>
 
-    <label htmlFor="caddress">Company Address </label>
-    <input type="text" id="caddress" name="companyaddress" placeholder="Company address.."/>
+                <label htmlFor="caddress">Company Address </label>
+                <input type="text" id="caddress" name="companyaddress" placeholder="Company address.."/>
 
-    <label htmlFor="email">Email Address </label>
-    <input type="text" id="email" name="emailaddress" placeholder="Email address.."/>
+                <label htmlFor="email">Email Address </label>
+                <input type="text" id="email" name="emailaddress" placeholder="Email address.."/>
 
-    <label htmlFor="phoneno">Phone No </label>
-    <input type="number" id="pnumber" name="phonenumber" placeholder="Phone Number.."/>
-  
-    <input type="submit" value="Submit"/>
-  </form>
-</div>
+                <label htmlFor="phoneno">Phone No </label>
+                <input type="number" id="pnumber" name="phonenumber" placeholder="Phone Number.."/>
+            
+                <input type="submit" value="Submit"/>
+            </form>
         </div>
+    </div>
     )
 }
 
