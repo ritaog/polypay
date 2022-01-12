ccd34ec50a4531825c2f552037c1b5e643db6f03
theowall <theo.wall11@gmail.com>
2022-01-11 17:29:54 -0700

    added browser router for profile page

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ProfilePage from "./pages/ProfilePage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
+  </BrowserRouter>,
+document.querySelector("#root"));
