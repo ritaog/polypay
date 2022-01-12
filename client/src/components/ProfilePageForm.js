a990346ee9db8f372de25312e806b12cfb912a09
theowall <theo.wall11@gmail.com>
2022-01-12 09:25:26 -0700

    corrected htmlFor

--- client/src/components/ProfilePageForm.js
+++ client/src/components/ProfilePageForm.js
@@ -8,19 +8,19 @@ const ProfilePageForm = () => {
             
         <div>
     <form>
-    <label htmlfor="name">Name</label>
+    <label htmlFor="name">Name</label>
     <input type="text" id="name" name="name" placeholder="Your name.."/>
 
-    <label htmlfor="cname">Company Name </label>
+    <label htmlFor="cname">Company Name </label>
     <input type="text" id="cname" name="companyname" placeholder="Company name.."/>
 
-    <label htmlfor="caddress">Company Address </label>
+    <label htmlFor="caddress">Company Address </label>
     <input type="text" id="caddress" name="companyaddress" placeholder="Company address.."/>
 
-    <label htmlfor="email">Email Address </label>
+    <label htmlFor="email">Email Address </label>
     <input type="text" id="email" name="emailaddress" placeholder="Email address.."/>
 
-    <label htmlfor="phoneno">Phone No </label>
+    <label htmlFor="phoneno">Phone No </label>
     <input type="number" id="pnumber" name="phonenumber" placeholder="Phone Number.."/>
   
     <input type="submit" value="Submit"/>
