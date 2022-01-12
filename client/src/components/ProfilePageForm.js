bcb52391fafae5a630f07edec56be2f8369f0d4e
Mahmuda <mahmuda2709@gmail.com>
2022-01-11 21:22:12 -0700

    added new line

--- client/src/components/ProfilePageForm.js
+++ client/src/components/ProfilePageForm.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import "./ProfilePageForm.css"
 
 const ProfilePageForm = () => {
     return (
-        <div>
+    <div>
             This is ProfilePageForm
+            
+        <div>
+    <form action="/action_page.php">
+    <label htmlfor="name">Name</label>
+    <input type="text" id="name" name="name" placeholder="Your name.."/>
+
+    <label htmlfor="cname">Company Name </label>
+    <input type="text" id="cname" name="companyname" placeholder="Company name.."/>
+
+    <label htmlfor="caddress">Company Address </label>
+    <input type="text" id="caddress" name="companyaddress" placeholder="Company address.."/>
+
+    <label htmlfor="email">Email Address </label>
+    <input type="text" id="email" name="emailaddress" placeholder="Email address.."/>
+
+    <label htmlfor="phoneno">Phone No </label>
+    <input type="number" id="pnumber" name="phonenumber" placeholder="Phone Number.."/>
+  
+    <input type="submit" value="Submit"/>
+  </form>
+</div>
         </div>
     )
 }
