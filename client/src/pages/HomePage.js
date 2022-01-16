3f063f329aa6d1b9fbbeb4ccf75cbd76fe911b22
theowall <theo.wall11@gmail.com>
2022-01-16 13:51:54 -0700

    moved fb token validation to back end

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,19 +2,26 @@ import { useState, useEffect } from 'react'
 import FacebookLogin from 'react-facebook-login'
 import axios from 'axios'
 
+
 const HomePage = ({ userData }) => {
   const [message, setMessage] = useState()
   const [accessToken, setAccessToken] = useState()
-  const [fbPageId, setFbPageId] = useState()
-  const [instaBuisnessId, setInstaBuisnessId] = useState()
-  // const componentClicked = (data) => {
-  //   console.log('data', data)
-  // }
 
   const responseFacebook = (response) => {
     console.log('fb response', response)
-    setAccessToken(response.accessToken)
+    setAccessToken(response)
   }
+
+  useEffect(() => {
+    const getData = async () => {
+      console.log(accessToken)
+      const response = await axios.post('auth/validateFb', accessToken)
+      console.log(response)
+    }
+    if (accessToken){
+      getData()
+    }
+  }, [accessToken])
 
   //grabs data from backend once page is rendered
   useEffect(() => {
@@ -24,53 +31,6 @@ const HomePage = ({ userData }) => {
     }
     getData()
   }, [])
-
-  //sends the access token to fb and returns the fb account ID
-  useEffect(() => {
-    let getFbId = async () => {
-      let response = await fetch(
-        `https://graph.facebook.com/v12.0/me/accounts?access_token=${accessToken}`
-      )
-      let responseJson = await response.json()
-      console.log('user page id:', responseJson)
-      return setFbPageId(responseJson.data[0].id)
-    }
-    if (accessToken) {
-      getFbId()
-    }
-  }, [accessToken])
-
-  // sends fb account Id and access token a has the linked instagram account page Id returned
-  useEffect(() => {
-    let getInstaBuisnessId = async () => {
-      let response = await fetch(
-        `https://graph.facebook.com/v12.0/${fbPageId}?fields=instagram_business_account&access_token=${accessToken}`
-      )
-      let responseJson = await response.json()
-      console.log('insta buisness page id:', responseJson)
-      return setInstaBuisnessId(responseJson.instagram_business_account.id)
-    }
-    if (fbPageId) {
-      getInstaBuisnessId()
-    }
-  }, [accessToken, fbPageId])
-
-  //         useEffect(() => {
-  //           let getInstaBuisnessId = async () => {
-  //             let response = await fetch(
-  //               `https://graph.facebook.com/v12.0/oauth/access_token?grant_type=fb_exchange_token&client_id=${client-id}&client_secret=${client-secret}&fb_exchange_token=${exchange-token}
-  // `
-  //             )
-  //             let responseJson = await response.json()
-  //             console.log('insta buisness page id:', responseJson)
-  //             return setInstaBuisnessId(
-  //               responseJson.instagram_business_account.id
-  //             )
-  //           }
-  //           if (fbPageId) {
-  //             getInstaBuisnessId()
-  //           }
-  //         }, [accessToken, fbPageId])
 
   return (
     <div>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import fetch from 'node-fetch'
 const router = express.Router()
 
 import passport from 'passport'
@@ -11,20 +12,21 @@ passport.use(
   new LocalStrategy(
     {
       usernameField: 'emailAddress',
-      passwordField: 'password'
-    }, 
+      passwordField: 'password',
+    },
     function (username, password, done) {
-    console.log('passport is trying to verify user', username)
-    VendorProfile.findUserByEmail(username)
-      .then((user) => {
-        if (!user || user.password !== password) {
-          done(null, false, { message: 'Incorrect username/password.' })
-          return
-        }
-        done(null, user)
-      })
-      .catch(done)
-  })
+      console.log('passport is trying to verify user', username)
+      VendorProfile.findUserByEmail(username)
+        .then((user) => {
+          if (!user || user.password !== password) {
+            done(null, false, { message: 'Incorrect username/password.' })
+            return
+          }
+          done(null, user)
+        })
+        .catch(done)
+    }
+  )
 )
 
 passport.serializeUser(function (user, done) {
@@ -45,10 +47,40 @@ passport.deserializeUser(function (id, done) {
     .catch(done)
 })
 
-router.post('/login', passport.authenticate('local'), async function (req, res) {
-  // If this function gets called, authentication was successful.
-  // `req.user` contains the authenticated user.
-  res.send(req.user)
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  async function (req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.send(req.user)
+  }
+)
+
+router.post('/validateFb', async function (req, res) {
+  let key = req.body
+  // console.log(process.env.FB_SECRET)
+  // console.log(key.accessToken)
+
+  let response1 = await fetch(
+    `https://graph.facebook.com/v12.0/me/accounts?access_token=${key.accessToken}`
+  )
+  let instaPageId = await response1.json()
+  let response2 = await fetch(
+    `https://graph.facebook.com/v12.0/${instaPageId.data[0].id}?fields=instagram_business_account&access_token=${key.accessToken}`
+  )
+  let instaBusiId = await response2.json()
+  let response3 = await fetch(
+    `https://graph.facebook.com/v12.0/oauth/access_token?grant_type=fb_exchange_token&client_id=${process.env.FB_APP_ID}&client_secret=${process.env.FB_SECRET}&fb_exchange_token=${key.accessToken}`
+  )
+  let permToken = await response3.json()
+  console.log('temp token:', key.accessToken)
+  console.log('user page id:', instaPageId.data[0].id)
+  console.log('insta busi account id:', instaBusiId.instagram_business_account.id)
+  console.log('insta permamnent token:', permToken.access_token)
+
+  res.sendStatus(200)
+
 })
 
 // router.get('/loggedInUser', function (req, res) {
