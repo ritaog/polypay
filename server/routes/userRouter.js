97109d5c3051a9a896553f9fc4db55ecb4168e38
theowall <theo.wall11@gmail.com>
2022-01-23 15:57:55 -0700

    add new userRouter.js file

---
+++ server/routes/userRouter.js
@@ -0,0 +1,39 @@
+import express from "express";
+import User from "../models/UserModel.js"
+
+
+const router = express.Router();
+
+// const mustBeLoggedIn = async (req, res, next) => {
+//   if (req.user) {
+//     next()
+//     return
+//   }
+//   res.sendStatus(401)
+// }
+ 
+//GET endpoint || description: http://localhost:5000/api/welcome
+router.get("/welcome", (_, res) => {
+  res.send("Hello World!!!!");
+});
+
+// POST endpoint || description: takes data from userForm and sends to DB
+router.post("/addUser", async (req, res) => {
+  // receives new profile data from front end
+  let incomingData = req.body;
+
+  // adds received data to User constructor
+  let newProfile = await new User(incomingData);
+
+  // saves new user data to users collection database
+  let newId = await newProfile.save()
+  console.log(newId);
+  
+  // sends id back to front end
+  res.json(newId);
+});
+
+
+
+
+export default router;
