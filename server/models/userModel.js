0c9f6b43c5cbcce695b3f1a9b2f28bbecf69562e
theowall <theo.wall11@gmail.com>
2022-01-23 16:15:55 -0700

    added new userModal.js

---
+++ server/models/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+// User Model Schema
+
+const UserSchema = new mongoose.Schema({
+  userName: { type: String, required: true },
+  userType: { type: String, required: true },
+  companyName: String,
+  companyAddress: String,
+  companyType: String,
+  emailAddress: { type: String, required: true },
+  password: { type: String, required: true },
+  phoneNo: Number,
+  userReview: Number,
+  instagramBusinessId: String,
+  permanentToken: String,
+  saleItems: [String],
+})
+
+export default mongoose.model('User', UserSchema)
