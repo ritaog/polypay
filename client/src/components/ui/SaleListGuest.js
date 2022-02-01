ffda841abea1b7ad0993d239c81b65b4f5e6504e
theowall <theo.wall11@gmail.com>
2022-02-01 09:36:23 -0700

    fixing gues image display

--- client/src/components/ui/SaleListGuest.js
+++ client/src/components/ui/SaleListGuest.js
@@ -5,21 +5,22 @@ import ImageListItemBar from '@mui/material/ImageListItemBar'
 import ListSubheader from '@mui/material/ListSubheader'
 import IconButton from '@mui/material/IconButton'
 import InfoIcon from '@mui/icons-material/Info'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 export default function SaleListGuest({ userData, profileId }) {
-  
   const [saleItems, setSaleItems] = useState([])
-  console.log("in guest", profileId.id)
+  console.log('in guest', profileId.id)
 
   useEffect(() => {
     const getSaleItemsByProfileId = async () => {
       const response = await axios.get(
         '/saleItem/listUserSaleItemsById/' + profileId.id
       )
-      console.log('responseGuest', response);
+      console.log('responseGuest', response)
       response.data.sort(function (a, b) {
         return new Date(b.postTime) - new Date(a.postTime)
       })
@@ -34,42 +35,43 @@ export default function SaleListGuest({ userData, profileId }) {
 
   return (
     <div>
-      <ImageList sx={{ width: 500, height: 1000 }}>
-        <ImageListItem key="Subheader" cols={2}>
-          <ListSubheader component="div">
-            {userData
-              ? userData.userName
-              : vendorName
-              ? vendorName.vendorName
+      <Card sx={{ maxWidth: 530 }}>
+        <CardContent sx={{ justifyContent: 'center' }}>
+          <ImageList sx={{ width: 500}}>
+            <ImageListItem key="Subheader" cols={2}>
+              <ListSubheader component="div">
+                { vendorName
+                  ? vendorName.vendorName
+                  : ''}
+              </ListSubheader>
+            </ImageListItem>
+            {saleItems
+              ? saleItems.map((item) => (
+                  <ImageListItem key={item.photos[0]}>
+                    <img
+                      src={`${item.photos[0]}?w=248&fit=crop&auto=format`}
+                      srcSet={`${item.photos[0]}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                      alt={item.description}
+                      loading="lazy"
+                    />
+                    <ImageListItemBar
+                      title={item.price}
+                      subtitle={item.quantity}
+                      actionIcon={
+                        <IconButton
+                          sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                          aria-label={`info about ${item.title}`}
+                        >
+                          <InfoIcon />
+                        </IconButton>
+                      }
+                    />
+                  </ImageListItem>
+                ))
               : ''}
-          </ListSubheader>
-        </ImageListItem>
-        {saleItems
-          ? saleItems.map((item) => (
-              <ImageListItem key={item.photos[0]}>
-                <img
-                  src={`${item.photos[0]}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item.photos[0]}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.description}
-                  loading="lazy"
-                />
-                <ImageListItemBar
-                  title={item.price}
-                  subtitle={item.quantity}
-                  actionIcon={
-                    <IconButton
-                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                      aria-label={`info about ${item.title}`}
-                    >
-                      <InfoIcon />
-                    </IconButton>
-                  }
-                />
-              </ImageListItem>
-            ))
-          : ''}
-      </ImageList>
+          </ImageList>
+        </CardContent>
+      </Card>
     </div>
   )
 }
-
