0142bb58e53248777574cf54ff22ad66f3c710fa
theowall <theo.wall11@gmail.com>
2022-02-03 12:53:40 -0700

    linked up button to upload images to library

--- client/src/components/imageUpload/PostScheduler.js
+++ client/src/components/imageUpload/PostScheduler.js
@@ -13,9 +13,35 @@ import ImageSelect from './ImageSelect'
 import PostInfo from './PostInfo'
 import Scheduler from './Scheduler'
 
-
-export default function PostScheduler({imageSelect}) {
+export default function PostScheduler({ imageSelect }) {
   const [activeStep, setActiveStep] = React.useState(0)
+
+  // const [title, setTitle] = useState('')
+  // const [price, setPrice] = useState('')
+  // const [quantity, setQuantity] = useState('')
+  // const [caption, setCaption] = useState('')
+  // const [about, setAbout] = useState('')
+  // const [canShip, setCanShip] = useState(false)
+  // const [location, setLocation] = useState('')
+  // const [postTime, setPostTime] = useState('')
+
+  // const scheduleHandler = async () => {
+  // shortcut to combine the response from first post and user data from user state to be sent to back end as one object
+  // const { instagramBusinessId, permanentToken, saleItems } = userData
+  // const saleItemDataBundle = {
+  //   ...responseUpload.data,
+  //   instagramBusinessId,
+  //   permanentToken,
+  //   saleItems,
+  // }
+  // console.log('saleItemDataBundle', saleItemDataBundle)
+  // second post sends combine object from above to back end to be posted to instagram and added to the users sale que
+  // const responseSchedule = await axios.post(
+  //   '/saleItem/schedule',
+  //   saleItemDataBundle
+  // )
+  // console.log(responseSchedule)
+  // }
 
   const handleNext = () => {
     setActiveStep(activeStep + 1)
@@ -25,7 +51,11 @@ export default function PostScheduler({imageSelect}) {
     setActiveStep(activeStep - 1)
   }
 
-  const steps = ['Choose Image', 'Quantity and Availability', 'Schedule Post Time']
+  const steps = [
+    'Choose Image',
+    'Quantity and Availability',
+    'Schedule Post Time',
+  ]
 
   function getStepContent(step) {
     switch (step) {
--- client/src/components/ui/DisplayMedia.js
+++ client/src/components/ui/DisplayMedia.js
@@ -15,6 +15,40 @@ import { useState, useEffect } from 'react'
 export default function SaleListUser({ userData, imageSelectHandler }) {
   const [saleItems, setSaleItems] = useState([])
 
+  const [uploadPhoto, setUploadPhoto] = useState([])
+
+  const postData = {
+    vendorName: userData.userName,
+    vendorId: userData._id,
+    postTitle: '',
+    price: 0,
+    quantity: 0,
+    photos: [],
+    description: '',
+    about: '',
+    canShip: false,
+    available: false,
+    postTime: new Date(),
+    location: '',
+  }
+
+  const uploadHandler = async (e) => {
+    // assigns a new form data constructor to append data to to send to back end. 'image' is file selected by user to be uploaded to instagram
+    // "formData" is all the data collected from the schedule post form to be sent to back end
+
+    console.log('uploadPhoto', e.target.files[0])
+    console.log('postData', postData)
+
+    const imageData = new FormData()
+    imageData.append('image', e.target.files[0])
+    imageData.append('formData', JSON.stringify(postData))
+
+    // first post sends photo file and form data to back end. response is the compiled sale item object with image url from cloudinary
+    const responseUpload = await axios.post('/saleItem/upload', imageData)
+
+    console.log(responseUpload)
+  }
+
   useEffect(() => {
     const getSaleItemsByLoggedUser = async () => {
       const response = await axios.get('/saleItem/listImagesByLoggedUser')
@@ -48,6 +82,9 @@ export default function SaleListUser({ userData, imageSelectHandler }) {
                 <Button
                   variant="contained"
                   component="label"
+                  onChange={(e) => {
+                    uploadHandler(e)
+                  }}
                   sx={{
                     backgroundColor: 'black',
                     '&:hover': { backgroundColor: 'darkGray' },
