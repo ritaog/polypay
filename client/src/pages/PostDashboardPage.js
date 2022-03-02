6deab7cf42ce280c4356b7fc1e71c64622f797a5
theowall <theo.wall11@gmail.com>
2022-03-02 14:03:32 -0700

    added stat bubbles

--- client/src/pages/PostDashboardPage.js
+++ client/src/pages/PostDashboardPage.js
@@ -1,39 +1,51 @@
 import * as React from 'react'
-// import { styled } from '@mui/material/styles'
-// import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
 import Masonry from '@mui/lab/Masonry'
-// import TimeLine from '../components/ui/TimeLine'
+import TimeLine from '../components/ui/TimeLine'
 import InstaRecentPosts from '../components/ui/InstaRecentPosts'
 // import AddImage from '../components/ui/AddImage'
 
-// const Item = styled(Paper)(({ theme }) => ({
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: 'center',
-//   color: theme.palette.text.secondary,
-// }))
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  borderRadius: '25px',
+  height: '70px'
+}))
 
 const PostDashboardPage = ({ userData }) => {
   return (
     <Box sx={{ width: '100%' }}>
-      {/* <div>Media Library</div> */}
-      <Masonry columns={2} spacing={2}>
-        {/* <Item sx={{ maxWidth: '50%', minWidth: '450px'}}> */}
-        {/* <DisplayMedia userData={userData} /> */}
-        {/* </Item> */}
-        {/* <Item
-          sx={{
-            maxWidth: '50%',
-            minWidth: '450px',
-            maxHeight: '750px',
-            overflowY: 'auto',
-          }}
-        > */}
-        {/* <TimeLine userData={userData} /> */}
-        {/* </Item> */}
-        <InstaRecentPosts userData={userData} />
-      </Masonry>
+      <Box sx={{ display: 'flex' }}>
+        <Box sx={{ width: '25%', padding: '0 20px 20px 0' }}>
+          <Item>hello</Item>
+        </Box>
+        <Box sx={{ width: '25%', padding: '0 20px 20px 0' }}>
+          <Item>hello</Item>
+        </Box>
+        <Box sx={{ width: '25%', padding: '0 20px 20px 0' }}>
+          <Item>hello</Item>
+        </Box>
+        <Box sx={{ width: '25%', padding: '0 20px 20px 0' }}>
+          <Item>hello</Item>
+        </Box>
+      </Box>
+
+      <InstaRecentPosts userData={userData} />
+      {/* <Item
+        sx={{
+          maxWidth: '50%',
+          minWidth: '450px',
+          maxHeight: '750px',
+          overflowY: 'auto',
+        }}
+      >
+        <TimeLine userData={userData} />
+      </Item> */}
+      {/* </Masonry> */}
     </Box>
   )
 }
