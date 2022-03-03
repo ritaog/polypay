f26715f35adf351f0af530d0ff879c1b41273676
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2022-03-03 13:45:38 -0700

    Creates new message after checkout

--- client/src/pages/StripeSuccessfulCheckout.js
+++ client/src/pages/StripeSuccessfulCheckout.js
@@ -3,8 +3,8 @@ const StripeSuccessfulCheckout = ({ userData }) => {
     <div>
       <h1>Thanks for your order!</h1>
       <p>
-        We appreciate your patronage! If you have any questions, please email
-        <a href={`mailto:${userData.emailAddress}`}>{userData.emailAddress}</a>
+        We appreciate your patronage! A receipt has been sent to the email
+        address you provided at checkout.
       </p>
     </div>
   )
